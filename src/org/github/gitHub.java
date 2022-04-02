bf5ff4243d308bea044c4261e859b9a774379716
PriyathR <102587525+PriyathR@users.noreply.github.com>
2022-04-02 15:59:28 +0530

    Update gitHub.java

--- src/org/github/gitHub.java
+++ src/org/github/gitHub.java
@@ -2,8 +2,22 @@ package org.github;
 
 public class gitHub {
 
+
 	  public void hub() {
 
 		  System.out.println("hub");
 		}
+	
+	 public void method1() {
+
+		  System.out.println("hub");
+		}
+	
+	 public void method2() {
+
+		  System.out.println("hub");
+		}
+	
+	
+	
 }
